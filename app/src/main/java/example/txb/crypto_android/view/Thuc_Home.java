15636dc38ef1dd2b40bc869818f481992bb5503c
xuanbinh2367 <xuanbinh236789@gmail.com>
2024-04-22 19:33:28 +0700

    project

---
+++ app/src/main/java/example/txb/crypto_android/view/Thuc_Home.java
@@ -0,0 +1,17 @@
+package example.txb.crypto_android.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import example.txb.crypto_android.R;
+
+
+public class Thuc_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thuc_activity_home);
+    }
+}
